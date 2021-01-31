b79cb28e2e2e5dcbaaa2646fd65e5830bc9905cf
edemio <77964475+edemio@users.noreply.github.com>
2021-01-31 20:16:26 +0100

    Add files via upload

--- random_name 2.py
+++ random_name 2.py
@@ -11,11 +11,10 @@ e_guesses_in_one_run = 0
 s_guesses_in_one_run = 0
 e_guess_list = []
 s_guess_list = []
-e_guesses_for_comp = 0
-s_guesses_for_comp = 0
+rounds = []
 e_smaller = 0
 s_smaller = 0
-while run <= 9:
+while run <= 99:
     while random_int1 + random_int2 != 19:
         random_int1 = 0 + randint(0, 13)
         random_int2 = 0 + randint(0, 13)
@@ -23,13 +22,11 @@ while run <= 9:
             print(random_int1 + random_int2)
             print('wow that is edems age')
             e_guess_list.append(e_guesses_in_one_run)
-            e_guesses_in_one_run = 0
         else:
             print(random_int1 + random_int2)
             print('try again')
             e_total_guesses += 1
             e_guesses_in_one_run += 1
-            e_guesses_for_comp = e_guesses_in_one_run
     while random_int1 + random_int2 != 21:
         random_int1 = 0 + randint(2, 13)
         random_int2 = 0 + randint(2, 13)
@@ -37,27 +34,31 @@ while run <= 9:
             print(random_int1 + random_int2)
             print('wow that is setors age')
             s_guess_list.append(s_guesses_in_one_run)
-            s_guesses_in_one_run = 0
         else:
             print(random_int1 + random_int2)
             print('try again')
             s_total_guesses += 1
             s_guesses_in_one_run += 1
-            s_guesses_for_comp = s_guesses_in_one_run
     run += 1
-    if e_guesses_for_comp < s_guesses_for_comp:
+    rounds.append(run)
+    if e_guesses_in_one_run < s_guesses_in_one_run:
         e_smaller += 1
-    elif e_guesses_for_comp == s_guesses_for_comp:
-        e_smaller += 0
-        s_smaller += 0
     else:
         s_smaller += 1
+    e_guesses_in_one_run = 0
+    s_guesses_in_one_run = 0
     print()
-    print(f'edem less guesses {e_smaller} / {run}')
-    print(f'setor less guesses {s_smaller} / {run}')
+    print(f'edem has less guesses in {e_smaller} / {run} cases')
+    print(f'setor has less guesses in {s_smaller} / {run} cases')
 print()
 print(f'list for edem guesses = {e_guess_list}')
 print(f'average tries edem = {sum(e_guess_list) / len(e_guess_list)}')
 print()
 print(f'list for setor guesses = {s_guess_list}')
 print(f'average tries setor = {sum(s_guess_list) / len(s_guess_list)}')
+print(((np.array(e_guess_list) - np.array(s_guess_list)) >= 0).sum())
+plt.plot(rounds, e_guess_list, s_guess_list)
+plt.show()
+total_guesses = e_total_guesses + s_total_guesses
+print(f'in {run} rounds a total amount of {total_guesses} were made. '
+      f'That are on average {total_guesses / run} guesses per round!')
